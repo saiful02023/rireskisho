6f234a908900c083a4364587dcdb205ad4b097ba
saiful <132709985+saiful02023@users.noreply.github.com>
2023-05-12 03:23:35 +0900

    Add files via upload

---
+++ portfolio/js/type.js
@@ -0,0 +1,7 @@
+var typed = new typed ('.type', {
+    strings: ["SoftwaerDeveloper", "WebDeveloper", "PythonExpert"],
+    typeSpeed : 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
---
+++ portfolio/restaurentwebsite/js/type.js
@@ -0,0 +1,13 @@
+/*============typed js =============*/
+var typed = new Typed (".type", {
+    strings: [
+        "イスラム", 
+        "エムデイ", 
+        "サイフル",
+
+    ],
+    typeSpeed : 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
